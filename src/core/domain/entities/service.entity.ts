68974f5aafb31c7b6dae36bc9d7b06586b220f4e
thayvitareli <thainyvitareli@hotmail.com>
2025-12-05 20:31:38 -0300

    refactor: domain service entity

--- src/core/domain/entities/service.entity.ts
+++ src/core/domain/entities/service.entity.ts
@@ -1,42 +1,37 @@
 export class Service {
-    private readonly _id: string;
-    private readonly _barbershopId: string;
-    private _name: string;
-    private _durationMinutes: number;
-    private _price: number;
-    private _active: boolean = true;
-
   constructor(
-    id: string,
-    barbershopId: string,
-    name: string,
-    durationMinutes: number,
-    price: number,
-    active: boolean = true,
+    private readonly _id: string,
+    private readonly _barbershopId: string,
+    private _name: string,
+    private _durationMinutes: number,
+    private _price: number,
+    private _active: boolean = true,
   ) {
-    if (!barbershopId) throw new Error('Barbershop ID cannot be empty.');
-    if (!name) throw new Error('Service name cannot be empty.');
-
-    if (durationMinutes <= 0)
+    if (!_barbershopId) throw new Error('Barbershop ID cannot be empty.');
+    if (!_name) throw new Error('Service name cannot be empty.');
+    if (_durationMinutes <= 0)
       throw new Error('Duration must be greater than 0.');
-
-    if (price < 0)
-      throw new Error('Price cannot be negative.');
-
-    this._id=id;
-    this._barbershopId=barbershopId;
-    this._name=name;
-    this._durationMinutes=durationMinutes; 
-    this._price=price; 
-    this._active = true
+    if (_price < 0) throw new Error('Price cannot be negative.');
   }
 
-  get id() { return this._id; }
-  get barbershopId() { return this._barbershopId; }
-  get name() { return this._name; }
-  get durationMinutes() { return this._durationMinutes; }
-  get price() { return this._price; }
-  get active() { return this._active; }
+  get id() {
+    return this._id;
+  }
+  get barbershopId() {
+    return this._barbershopId;
+  }
+  get name() {
+    return this._name;
+  }
+  get durationMinutes() {
+    return this._durationMinutes;
+  }
+  get price() {
+    return this._price;
+  }
+  get active() {
+    return this._active;
+  }
 
   updatePrice(price: number) {
     if (price < 0) throw new Error('Price cannot be negative.');
@@ -45,7 +40,7 @@ export class Service {
 
   updateDuration(duration: number) {
     if (duration <= 0) throw new Error('Duration must be > 0.');
-   
+
     this._durationMinutes = duration;
   }
 
