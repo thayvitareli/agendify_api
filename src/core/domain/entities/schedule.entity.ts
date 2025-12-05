61249d7c8cd35e0cbebc6ed0637be5e1596b7afd
thayvitareli <thainyvitareli@hotmail.com>
2025-12-05 20:34:13 -0300

    refactor: domain schedule entity and test

--- src/core/domain/entities/schedule.entity.ts
+++ src/core/domain/entities/schedule.entity.ts
@@ -1,29 +1,15 @@
 export class Schedule {
-   private readonly _id: string;
-    private readonly _barbershopId: string;
-    private readonly _customerId: string;
-    private readonly _serviceId: string;
-    private readonly _startAt: Date;
-    private readonly _endAt: Date;
-    private _status: 'PENDING' | 'CONFIRMED' | 'CANCELED';
-    private _canceledAt?: Date | null;
-
   constructor(
-   id:string,
-   barbershopId: string,
-   customerId:string,
-   serviceId:string,
-   startAt:Date,
-   endAt:Date,
+    private readonly _id: string,
+    private readonly _barbershopId: string,
+    private readonly _customerId: string,
+    private readonly _serviceId: string,
+    private readonly _startAt: Date,
+    private readonly _endAt: Date,
+    private _status: 'PENDING' | 'CONFIRMED' | 'CANCELED' = 'PENDING',
+    private _canceledAt?: Date | null,
   ) {
-    if (endAt <= startAt) throw new Error('End must be after start.');
-
-    this._id = id;
-    this._barbershopId = barbershopId;
-    this._customerId = customerId;
-    this._serviceId = serviceId;
-    this._startAt = startAt;
-    this._endAt = endAt
+    if (_endAt <= _startAt) throw new Error('End must be after start.');
   }
 
   cancel() {
@@ -34,14 +20,28 @@ export class Schedule {
     this._canceledAt = new Date();
   }
 
-  get id() { return this._id; }
-  get startAt() { return this._startAt; }
-  get endAt() { return this._endAt; }
-  get barbershopId() { return this._barbershopId; }
-  get customerId() { return this._customerId; }
-  get serviceId() { return this._serviceId; }
-  get canceledAt() { return this._canceledAt; }
-  get status() { return this._status; }
-
-
+  get id() {
+    return this._id;
+  }
+  get startAt() {
+    return this._startAt;
+  }
+  get endAt() {
+    return this._endAt;
+  }
+  get barbershopId() {
+    return this._barbershopId;
+  }
+  get customerId() {
+    return this._customerId;
+  }
+  get serviceId() {
+    return this._serviceId;
+  }
+  get canceledAt() {
+    return this._canceledAt;
+  }
+  get status() {
+    return this._status;
+  }
 }
--- test/unit/schedule.spec.ts
+++ test/unit/schedule.spec.ts
@@ -1,55 +1,76 @@
-import e from 'express';
-import {Schedule} from '../../src/core/domain/entities/schedule.entity';
-
+import { Schedule } from '../../src/core/domain/entities/schedule.entity';
 
 describe('Schedule entity', () => {
-
   it('Should throw an error if end is after start', () => {
-      const startDate = new Date('2025-02-01 13:00')
-      const endDate = new Date('2025-02-01 12:00')
+    const startDate = new Date('2025-02-01 13:00');
+    const endDate = new Date('2025-02-01 12:00');
 
-      expect(()=> new Schedule('shId', 'barbershopId','customerId', 'serviceId',startDate,endDate )).toThrow('End must be after start.')
-  });
-  
-    it('Should instance a new Schedule', () => {
-      const startDate = new Date('2025-02-01 13:00')
-      const endDate = new Date('2025-02-01 13:30')
-
-      const shedule = new Schedule('shId', 'barbershopId','customerId', 'serviceId',startDate,endDate)
-      
-      expect(shedule.id).toBe('shId')
-      expect(shedule.barbershopId).toBe('barbershopId')
-      expect(shedule.serviceId).toBe('serviceId')
-      expect(shedule.startAt).toBe(startDate)
-      expect(shedule.endAt).toBe(endDate)
-
-
+    expect(
+      () =>
+        new Schedule(
+          'shId',
+          'barbershopId',
+          'customerId',
+          'serviceId',
+          startDate,
+          endDate,
+        ),
+    ).toThrow('End must be after start.');
   });
 
-  
-    it('Should throw an error if try cancel a schedule cancelled', () => {
-      const startDate = new Date('2025-02-01 13:00')
-      const endDate = new Date('2025-02-01 13:30')
+  it('Should instance a new Schedule', () => {
+    const startDate = new Date('2025-02-01 13:00');
+    const endDate = new Date('2025-02-01 13:30');
 
-      const shedule = new Schedule('shId', 'barbershopId','customerId', 'serviceId',startDate,endDate)
+    const shedule = new Schedule(
+      'shId',
+      'barbershopId',
+      'customerId',
+      'serviceId',
+      startDate,
+      endDate,
+    );
 
-      shedule.cancel()
+    expect(shedule.id).toBe('shId');
+    expect(shedule.barbershopId).toBe('barbershopId');
+    expect(shedule.serviceId).toBe('serviceId');
+    expect(shedule.startAt).toBe(startDate);
+    expect(shedule.endAt).toBe(endDate);
+  });
 
-      expect(()=> shedule.cancel()).toThrow('Schedule already canceled.')
-   });
+  it('Should throw an error if try cancel a schedule cancelled', () => {
+    const startDate = new Date('2025-02-01 13:00');
+    const endDate = new Date('2025-02-01 13:30');
 
-   
-    it('Should cancel a schedule', () => {
-      const startDate = new Date('2025-02-01 13:00')
-      const endDate = new Date('2025-02-01 13:30')
+    const shedule = new Schedule(
+      'shId',
+      'barbershopId',
+      'customerId',
+      'serviceId',
+      startDate,
+      endDate,
+    );
 
-      const shedule = new Schedule('shId', 'barbershopId','customerId', 'serviceId',startDate,endDate)
+    shedule.cancel();
 
-      shedule.cancel()
+    expect(() => shedule.cancel()).toThrow('Schedule already canceled.');
+  });
 
-      expect(shedule.status).toBe('CANCELED')
-   });
+  it('Should cancel a schedule', () => {
+    const startDate = new Date('2025-02-01 13:00');
+    const endDate = new Date('2025-02-01 13:30');
 
+    const shedule = new Schedule(
+      'shId',
+      'barbershopId',
+      'customerId',
+      'serviceId',
+      startDate,
+      endDate,
+    );
 
+    shedule.cancel();
 
+    expect(shedule.status).toBe('CANCELED');
+  });
 });
