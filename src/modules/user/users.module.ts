c66c72deab0dbcbfbcd58abcb299c969b1a304c9
thayvitareli <thainyvitareli@hotmail.com>
2025-12-08 20:53:50 -0300

    refactor: user module

--- src/modules/user/users.module.ts
+++ src/modules/user/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import CreateUserUseCase from './use-cases/create-user.use-case';
 import { UserController } from './presentation/controllers/user.controller';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],
   controllers: [UserController],
-  providers: [CreateUserUseCase],
 })
 export class UserModule {}
