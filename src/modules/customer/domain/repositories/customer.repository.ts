110d14c6ab3d0a9dc4dace7480fa7ae8fda8def1
thayvitareli <thainyvitareli@hotmail.com>
2025-12-26 18:05:38 -0300

    refactor: Customer domain repository

--- src/modules/customer/domain/repositories/customer.repository.ts
+++ src/modules/customer/domain/repositories/customer.repository.ts
@@ -1,7 +1,7 @@
 import { Customer } from '../model/customer.model';
 
 export interface ICustomerRepository {
-  save(user: Customer): Promise<void>;
+  save(customer: Customer): Promise<Customer | null>;
   findById(id: string): Promise<Customer | null>;
   findByEmail(email: string): Promise<Customer | null>;
 }
