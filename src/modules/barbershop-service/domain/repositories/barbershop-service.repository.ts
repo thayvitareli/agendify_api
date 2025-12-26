e3878a8828e4c15e4618af9bb2e201b05cc5c26c
thayvitareli <thainyvitareli@hotmail.com>
2025-12-26 19:14:56 -0300

    refactor: barbershop service domain repository (save method return)

--- src/modules/barbershop-service/domain/repositories/barbershop-service.repository.ts
+++ src/modules/barbershop-service/domain/repositories/barbershop-service.repository.ts
@@ -1,7 +1,7 @@
 import { BarbershopService } from '../model/barbershop-service.model';
 
 export interface IBarbershopServiceRepository {
-  save(service: BarbershopService): Promise<void>;
+  save(service: BarbershopService): Promise<BarbershopService | null>;
   findById(id: string): Promise<BarbershopService | null>;
   findByBarbershopId(barbershopId: string): Promise<BarbershopService[]>;
 }
