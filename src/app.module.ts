d36e5f122df297e9d6722ee6e30d2efa6c807362
thayvitareli <thainyvitareli@hotmail.com>
2025-12-27 23:28:30 -0300

    chore: config TypeOrmModule on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { BookingModule } from './modules/booking/booking.module';
 import { CustomerModule } from './modules/customer/customer.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from './modules/customer/infrastructure/entity/customer.entity';
+import { UserEntity } from './modules/user/infrastructure/entity/user.entity';
+import { BookingEntity } from './modules/booking/infrastructure/entity/booking.entity';
+import { BarbershopEntity } from './modules/barbershop/infrastructure/entity/barbershop.entity';
+import { BarbershopServiceEntity } from './modules/barbershop-service/infrastructure/entity/barbershop-service.entity';
 
 @Module({
-  imports: [CustomerModule, BookingModule, AuthModule],
+  imports: [
+    CustomerModule,
+    BookingModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: [
+        CustomerEntity,
+        UserEntity,
+        BookingEntity,
+        BarbershopEntity,
+        BarbershopServiceEntity,
+      ],
+      synchronize: true,
+      dropSchema: true,
+      logging: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
