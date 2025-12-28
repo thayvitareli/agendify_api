7f045cb180ad89cf7b1ab99faa0672b1ed87aa28
thayvitareli <thainyvitareli@hotmail.com>
2025-12-28 15:25:38 -0300

    refactor: constructor on RegisterServiceUseCase

--- src/modules/barbershop-service/use-cases/register-service.use-case.ts
+++ src/modules/barbershop-service/use-cases/register-service.use-case.ts
@@ -1,20 +1,19 @@
-import { IBarbershopRepository } from 'src/modules/barbershop/domain/repositories/barbershop.repository';
+import type { IBarbershopRepository } from 'src/modules/barbershop/domain/repositories/barbershop.repository';
 import { BarbershopService } from '../domain/model/barbershop-service.model';
-import { IBarbershopServiceRepository } from '../domain/repositories/barbershop-service.repository';
+import type { IBarbershopServiceRepository } from '../domain/repositories/barbershop-service.repository';
+import { RegisterServiceDto } from '../presentation/dto/register-service.dto';
+import { v4 as uuid } from 'uuid';
+import { Inject } from '@nestjs/common';
 
 export class RegisterServiceUseCase {
   constructor(
+    @Inject('IBarbershopRepository')
     private readonly barbershopRepo: IBarbershopRepository,
+    @Inject('IBarbershopServiceRepository')
     private readonly barbershopServiceRepo: IBarbershopServiceRepository,
   ) {}
 
-  async execute(input: {
-    id: string;
-    barbershopId: string;
-    name: string;
-    durationMinutes: number;
-    price: number;
-  }) {
+  async execute(input: RegisterServiceDto) {
     const barbershop = await this.barbershopRepo.findById(input.barbershopId);
 
     if (!barbershop) {
@@ -22,10 +21,10 @@ export class RegisterServiceUseCase {
     }
 
     const service = new BarbershopService(
-      input.id,
+      uuid(),
       input.barbershopId,
       input.name,
-      input.durationMinutes,
+      input.durationInMinutes,
       input.price,
     );
 
