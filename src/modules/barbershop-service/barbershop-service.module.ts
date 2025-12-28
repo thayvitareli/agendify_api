127bde643574f0784919595fe2df533be7e64c1b
thayvitareli <thainyvitareli@hotmail.com>
2025-12-28 15:25:16 -0300

    refactor: imports and providers on BarbershopServiceModule

--- src/modules/barbershop-service/barbershop-service.module.ts
+++ src/modules/barbershop-service/barbershop-service.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BarbershopServiceController } from './presentation/controller/barbershop-service.controller';
+import { TypeORMBarbershopServiceRepository } from './infrastructure/repositories/typeorm-barbershop-service.repository';
+import { TypeORMBarbershopRepository } from '../barbershop/infrastructure/repositories/typeorm-barbershop.repository';
+import { RegisterServiceUseCase } from './use-cases/register-service.use-case';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BarbershopServiceEntity } from './infrastructure/entity/barbershop-service.entity';
+import { BarbershopEntity } from '../barbershop/infrastructure/entity/barbershop.entity';
 
 @Module({
   controllers: [BarbershopServiceController],
+  imports: [TypeOrmModule.forFeature([BarbershopEntity, BarbershopServiceEntity])],
+  providers: [
+    RegisterServiceUseCase,
+    {
+      provide: 'IBarbershopServiceRepository',
+      useClass: TypeORMBarbershopServiceRepository,
+    },
+    {
+      provide: 'IBarbershopRepository',
+      useClass: TypeORMBarbershopRepository,
+    },
+  ],
 })
-export class BarbershopModule {}
+export class BarbershopServiceModule {}
