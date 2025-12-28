99fa8d6b84c6e335804a9d82c2ef8b86b06bf2f9
thayvitareli <thainyvitareli@hotmail.com>
2025-12-28 16:08:49 -0300

    refactor:RegisterBookingDto

--- src/modules/booking/presentation/dtos/register-booking.dto.ts
+++ src/modules/booking/presentation/dtos/register-booking.dto.ts
@@ -1,6 +1,16 @@
+import { Transform } from 'class-transformer';
+import { IsString } from 'class-validator';
+
 export class RegisterBookingDto {
+  @IsString()
   barbershopId: string;
+
+  @IsString()
   customerId: string;
+
+  @IsString()
   serviceId: string;
+
+  @Transform(({ value }) => new Date(value))
   startAt: Date;
 }
