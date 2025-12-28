02f1d41b095c189d27883f6c7c2697b0980a2d6d
thayvitareli <thainyvitareli@hotmail.com>
2025-12-28 16:07:48 -0300

    fix: imports and provides on booking mocule

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
 import { BookingController } from './presentation/controllers/booking.controller';
+import { TypeORMBookingRepository } from './infrastructure/repositories/typeorm-booking.repository';
+import { RegisterBookingUseCase } from './use-cases/register-booking.use-case';
+import { TypeORMCustomerRepository } from '../customer/infrastructure/repositories/typeorm-customer.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from '../customer/infrastructure/entity/customer.entity';
+import { BookingEntity } from './infrastructure/entity/booking.entity';
+import { BarbershopServiceEntity } from '../barbershop-service/infrastructure/entity/barbershop-service.entity';
+import { TypeORMBarbershopServiceRepository } from '../barbershop-service/infrastructure/repositories/typeorm-barbershop-service.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      CustomerEntity,
+      BookingEntity,
+      BarbershopServiceEntity,
+    ]),
+  ],
   controllers: [BookingController],
+  providers: [
+    RegisterBookingUseCase,
+    { provide: 'IBookingRepository', useClass: TypeORMBookingRepository },
+    { provide: 'ICustomerRepository', useClass: TypeORMCustomerRepository },
+    {
+      provide: 'IBarbershopServiceRepository',
+      useClass: TypeORMBarbershopServiceRepository,
+    },
+  ],
 })
 export class BookingModule {}
