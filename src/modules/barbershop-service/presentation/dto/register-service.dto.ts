725608ce147a4f7e917b2fbdb5959a5fdbd8a7a6
thayvitareli <thainyvitareli@hotmail.com>
2026-01-07 21:12:01 -0300

    refactor: add validation on register service dto

--- src/modules/barbershop-service/presentation/dto/register-service.dto.ts
+++ src/modules/barbershop-service/presentation/dto/register-service.dto.ts
@@ -1,6 +1,18 @@
+import { Transform } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+
 export class RegisterServiceDto {
+  @IsString()
   name: string;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
   durationInMinutes: number;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
   price: number;
+
+  @IsString()
   barbershopId: string;
 }
